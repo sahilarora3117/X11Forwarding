b5d4aab2f7d671972125f8e7cf19ea65b179e2e4
sahilarora3117 <iamsahil@protonmail.com>
2021-10-05 12:18:38 +0530

    changed theming and erros

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,40 +1,111 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.global.css';
 const electron = window.require('electron');
-const startSession = (_IP: string, _PORT: string, _USERNAME: string, _PASSWORD: string, _COMMAND: string) => {
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+const startSession = (
+  _IP: string,
+  _PORT: string,
+  _USERNAME: string,
+  _PASSWORD: string,
+  _COMMAND: string
+) => {
   var ses = {
     ip: _IP,
     port: _PORT,
     username: _USERNAME,
     password: _PASSWORD,
-    command: _COMMAND
-  }
+    command: _COMMAND,
+  };
   electron.ipcRenderer.send('ipc-example', ses);
-
-}
-
+};
 
 const Hello = () => {
-  const [IP, setIP] = useState("");
-  const [PORT, setPORT] = useState("");
-  const [USERNAME, setUSERNAME] = useState("");
-  const [PASSWORD, setPASSWORD] = useState("");
-  const [COMMAND, setCOMMAND] = useState("");
+  const [IP, setIP] = useState('');
+  const [PORT, setPORT] = useState('');
+  const [USERNAME, setUSERNAME] = useState('');
+  const [PASSWORD, setPASSWORD] = useState('');
+  const [COMMAND, setCOMMAND] = useState('');
   return (
-    <div>
-      <h1>
-        Forward X11 Server
-      </h1>
-      <form>
-        <input id="standard-basic" type="text" onChange={e => setIP(e.target.value)} placeholder="IP" required/>
-        <input id="standard-basic" type="number" onChange={e => setPORT(e.target.value)} placeholder="port" required/><br />
-        <input id="standard-basic" type="text" onChange={e => setUSERNAME(e.target.value)} placeholder="username" required/>
-        <input id="standard-basic" type="password" onChange={e => setPASSWORD(e.target.value)} placeholder="password"/><br />
-        <input id="standard-full-width"  type="text" onChange={e => setCOMMAND(e.target.value)} placeholder="command" required/><br />
-        <button type="submit" onClick={() => startSession(IP, PORT, USERNAME, PASSWORD, COMMAND)}>Start Session</button>
-      </form>
-    </div>
+    <>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <h1>Forward X11 Server</h1>
+      </Container>
+      
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="on"
+      >
+        <TextField
+          id="outlined-basic"
+          type="text"
+          onChange={(e) => setIP(e.target.value)}
+          label="IP"
+        />
+        <TextField
+          id="outlined-basic"
+          type="number"
+          onChange={(e) => setPORT(e.target.value)}
+          label="Port"
+        />
+        <br />
+        <TextField
+          id="outlined-basic"
+          type="text"
+          onChange={(e) => setUSERNAME(e.target.value)}
+          label="Username"
+        />
+        <TextField
+          id="outlined-basic"
+          type="password"
+          onChange={(e) => setPASSWORD(e.target.value)}
+          label="Password"
+        />
+        <br />
+        <TextField
+          id="outlined-multiline-flexible"
+          multiline
+          type="text"
+          onChange={(e) => setCOMMAND(e.target.value)}
+          label="Command"
+        />
+        <br />
+      </Box>
+      </Container>
+      <Container
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+      <button
+        type="submit"
+        onClick={() => startSession(IP, PORT, USERNAME, PASSWORD, COMMAND)}
+      >
+        Start Session
+      </button>
+      </Container>
+    </>
   );
 };
 
